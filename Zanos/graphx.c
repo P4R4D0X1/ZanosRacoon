99ab298d69c5314db413b678508a8bcb440d4eaa
Aurore <Aurore@DESKTOP-SQVIIBU>
2016-05-23 22:46:08 +0200

    Début d'implémentation de displayMap()

--- Zanos/graphx.c
+++ Zanos/graphx.c
@@ -7,5 +7,16 @@ int loadInterface(sInterface *p_interface) {
 }
 
 int displayMap(sInterface *p_interface, sMap *p_map) {
+	int i, j;
+
+	for (i = 0; i < p_map->mapDimension.height; ++i) {
+		for (j = 0; j < p_map->mapDimension.width; ++j) {
+			p_interface->caseSprite[p_map->path[i][j].type];
+
+
+		}
+
+	}
+
 
 }
