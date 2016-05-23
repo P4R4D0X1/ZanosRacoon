485f6349115e9a049e624f2188d6bef7e966f2a4
Aurore <Aurore@DESKTOP-SQVIIBU>
2016-05-23 21:56:37 +0200

    Modifications tCase en t_case, pareil pour player

--- Zanos/graphx.h
+++ Zanos/graphx.h
@@ -11,15 +11,14 @@
 #include "include\SDL2\SDL_image.h"
 
 
-
 typedef struct s_interface {
 	sPosition origin;
-	SDL_Texture **tCase;
+	SDL_Texture **t_case;
 } sInterface;
 
 
 typedef struct s_player {
-	SDL_Texture ** player;
+	SDL_Texture **t_player;
 	sPosition position;
 } sPlayer;
 
