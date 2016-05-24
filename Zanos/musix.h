6701695c25539e71366cf4c35ca93bee02049954
Aurore <Aurore@DESKTOP-SQVIIBU>
2016-05-24 12:05:30 +0200

    Création fonction playSonor()

--- Zanos/musix.h
+++ Zanos/musix.h
@@ -8,5 +8,14 @@
 #include "metric.h"
 #include "map.h"
 
+#include "include\SDL2\SDL.h"
+#include "include\SDL2\SDL_image.h"
+#include "include\SDL2\SDL_mixer.h"
+
+typedef struct s_sonor {
+	Mix_Music *music;
+}sSonor;
+
+void playSonor(sSonor *p_sonor);
 
 #endif
