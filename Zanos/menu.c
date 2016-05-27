cb3f82e4f12a13c82c85aa4f9769b837fdd16c8a
Paradox <paradoxone1@gmail.com>
2016-05-27 10:48:57 +0200

    Ameilloraton du tutoriel

--- Zanos/menu.c
+++ Zanos/menu.c
@@ -75,8 +75,9 @@ void createMenu() {
 	loadAnimation(1, &l_raccoon, 3, l_posRaccoon, "./assets/sprite/anim/raccoon-skate_", &l_interface, 10);
 	loadAnimation(1, &l_logo, 2, l_posLogo, "./assets/sprite/anim/raccoonzanos_", &l_interface, 100);
 	loadAnimation(1, &l_play, 4, l_posPlay, "./assets/sprite/anim/play_", &l_interface, 5);
-	loadAnimation(1, &l_tuto, 6, l_posBG, "./assets/sprite/anim/tuto_", &l_interface, 120);
+	loadAnimation(1, &l_tuto, 8, l_posBG, "./assets/sprite/anim/tuto_", &l_interface, 120);
 
+	Mix_PlayMusic(l_interface.sonor.musicMenu, -1);
 
 	while (l_loop) {
 
@@ -92,6 +93,9 @@ void createMenu() {
 							SDL_Delay(SDL_ANIMATION_FRAMETIME);
 						} while (l_tuto->actualFrame != 0);
 
+						Mix_PauseMusic();
+						Mix_PlayMusic(l_interface.sonor.musicGame, -1);
+
 						loadMap(&l_map, "map0.txt");
 						generateGraph(l_map);
 						gameLoop(&l_interface, l_map);
@@ -106,7 +110,7 @@ void createMenu() {
 						generateGraph(l_map);
 						gameLoop(&l_interface, l_map);
 						free(l_map);
-
+						Mix_PauseMusic();
 					}
 					break;
 
